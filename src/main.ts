62a16ccdec28467f4b761a33e73eea7d74e699c0
Rômulo Gabriel <romulogabrielcavacante@gmail.com>
2023-09-14 11:56:58 -0400

    integração ao banco de dados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
+  console.log("password",process.env.DB_PASSWORD);
   const app = await NestFactory.create(AppModule);
+  
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { createUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
 
+
+        constructor(private readonly userService: UserService){}
     @Post()
-    async createUser(@Body() createUser: createUserDto) {
-        return {
-            ...createUser,
-            password: undefined,
-        }
+    async createUser(@Body() createUser: createUserDto): Promise<UserEntity> {
+        return this.userService.createUser(createUser);
     }
+
+    @Get()
+    async getAllUser(): Promise<UserEntity[]> {
+        return this.userService.getAllUser()
+    }
+
 }
+
+    
