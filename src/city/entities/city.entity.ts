e5ca71ff4a25b91a922c4d0b06ebdf25f48eadd0
Rômulo Gabriel <romulogabrielcavacante@gmail.com>
2023-09-18 14:53:42 -0400

    add cache

--- src/city/entities/city.entity.ts
+++ src/city/entities/city.entity.ts
@@ -1,6 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity({ name: 'address'})
+@Entity({ name: 'city'})
 export class CityEntity {
     @PrimaryGeneratedColumn('rowid')
     id: number;
