d6d3058a6f7d33d92f3e94362460f67fa19904e2
Rômulo Gabriel <romulogabrielcavacante@gmail.com>
2023-09-26 11:02:23 -0400

    feat:create jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,16 +4,20 @@ import { LoginDto } from './dtos/login.dto';
 import { UserService } from 'src/user/user.service';
 import { NotFoundError } from 'rxjs';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnAddressDto } from 'src/address/dtos/returnAddress.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
+import { ReturnLogin } from './dtos/returnLogin.dto';
 
 @Injectable()
 export class AuthService {
     
 constructor(
-    private readonly userService: UserService,
-) {}
+    private readonly userService: UserService, private jwtService: JwtService) {}
 
-    async login(loginDto: LoginDto): Promise<UserEntity> {
-        const user: UserEntity = await this.userService
+    async login(loginDto: LoginDto): Promise<ReturnLogin> {
+        const user: UserEntity | undefined = await this.userService
         .findUserByEmail(loginDto.email)
         .catch(() => undefined);
 
@@ -25,6 +29,9 @@ constructor(
             throw new NotFoundException('Email or passord invalid')
         }
 
-        return user;
+        return {
+            accessToken: this.jwtService.sign({...new LoginPayload(user)}),
+            user: new ReturnUserDto(user),
+        };
     }
 }
